57cd80fa3374215b0984f87acfec8b8ae33f4791
leosfreitas <leonardosf@al.insper.edu.br>
2024-02-15 11:32:16 -0300

    commit

--- classes.py
+++ classes.py
@@ -4,9 +4,9 @@ class Jogo:
     def __init__(self):
         pygame.init()
         pygame.display.set_caption('APS1 ALG LIN')
-        self.window = pygame.display.set_mode((800,600))
-        self.fundo = pygame.image.load('assets\img/ceu-estrelado.jpeg')
-        self.passaro = pygame.image.load('assets\img/angry-birds-png.png')
+        self.window = pygame.display.set_mode((1200,800))
+        img_fundo = pygame.image.load('assets\img/ceu-estrelado.jpeg')
+        self.fundo = pygame.transform.scale(img_fundo, (1200,800))
         self.porco = pygame.image.load('assets\img/porco-angry-birds.png')
 
     def roda(self):
@@ -24,7 +24,7 @@ class Canhao:
         img_canhao = pygame.image.load('assets\img/rocket-launcher.png')
         self.canhao_original = pygame.transform.scale(img_canhao, (200,100))
         self.canhao_rotacionado = self.canhao_original
-        self.pos_canhao = (10,490)
+        self.pos_canhao = (10,600)
 
     def desenha_canhao(self,window):
         window.blit(self.canhao_rotacionado, self.pos_canhao)
@@ -35,6 +35,22 @@ class Canhao:
         angle = math.degrees(math.atan2(-rel_y, rel_x))
         self.canhao_rotacionado = pygame.transform.rotate(self.canhao_original, angle)
 
+class Passaro: 
+    def __init__(self):
+        img_passaro = pygame.image.load('assets\img/angry-birds-png.png')
+        self.passaro = pygame.transform.scale(img_passaro, (50,50))
+        self.rect_passaro = self.passaro.get_rect()
+        self.condicao_passaro = False
+
+    def desenha_disparo(self,window):
+        if self.condicao_passaro:
+            window.blit(self.passaro, self.rect_passaro)
+
+    def atira_disparo(self):
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        rel_x, rel_y = mouse_x - self.pos_disparo[0], mouse_y - self.pos_disparo[1]
+        angle = math.degrees(math.atan2(-rel_y, rel_x))
+        self.disparo = pygame.transform.rotate(self.disparo, angle)
 
 class TelaJogo(Jogo):
     def __init__(self):
