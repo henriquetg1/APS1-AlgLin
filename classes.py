b71096e5df8b4fca8788b1fe80ff5b8019c0f162
henriquetg1 <henriqueturco1@gmail.com>
2024-02-15 11:32:57 -0300

    passaro_projetil

--- classes.py
+++ classes.py
@@ -8,6 +8,7 @@ class Jogo:
         self.fundo = pygame.image.load('assets\img/ceu-estrelado.jpeg')
         self.passaro = pygame.image.load('assets\img/angry-birds-png.png')
         self.porco = pygame.image.load('assets\img/porco-angry-birds.png')
+        self.tiros_passaros = []
 
     def roda(self):
         self.desenha()
@@ -18,6 +19,16 @@ class Jogo:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                self.atira(mouse_x, mouse_y)
+
+    def atira(self, x, y):
+        distancia_x = x - 100
+        distancia_y = y - 100
+        d = math.sqrt(distancia_x**2 + distancia_y**2)
+        # Velocidade com base na distância do mouse
+        velocidade = min(10, d/10)
 
 class TelaJogo(Jogo):
     def __init__(self):
@@ -29,13 +40,31 @@ class TelaJogo(Jogo):
         self.window.blit(self.passaro, (100,100))
         self.window.blit(self.porco, (600,100))
 
-
     def update(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
         return self 
+
+class Passaro_Projetil:
+    def __init__(self, origem_x, origem_y, destino_x, destino_y, velocidade):
+        self.x = origem_x
+        self.y = origem_y
+        self.destino_x = destino_x
+        self.destino_y = destino_y
+        self.velocidade = velocidade
+
+    def movimento(self):
+        distancia_x = self.destino_x - self.x
+        distancia_y = self.destino_y - self.y
+        modulo = math.sqrt(distancia_x**2 + distancia_y**2)
+        if modulo > self.velocidade:
+            self.x += distancia_x * self.velocidade / d
+            self.y += distancia_y * self.velocidade / d
+        else:
+            self.x = self.destino_x
+            self.y = self.destino_y
     
 tela_atual = TelaJogo()
 while True:
