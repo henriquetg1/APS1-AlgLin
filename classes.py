a3125027010ba630d2f46415c1234aa094439558
leosfreitas <leonardosf@al.insper.edu.br>
2024-02-20 16:01:28 -0300

    .

--- classes.py
+++ classes.py
@@ -8,7 +8,11 @@ class Jogo:
         img_fundo = pygame.image.load('assets\img/ceu-estrelado.jpeg')
         self.fundo = pygame.transform.scale(img_fundo, (1200,800))
         self.porco = pygame.image.load('assets\img/porco-angry-birds.png')
-        self.tiros_passaros = []
+        
+        img_passaro = pygame.image.load('assets\img/angry-birds-png.png')
+        self.passaro = pygame.transform.scale(img_passaro, (50,50))
+        self.rect_passaro = self.passaro.get_rect()
+        self.condicao_passaro = False
 
     def roda(self):
         self.desenha()
@@ -19,16 +23,6 @@ class Jogo:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                mouse_x, mouse_y = pygame.mouse.get_pos()
-                self.atira(mouse_x, mouse_y)
-
-    def atira(self, x, y):
-        distancia_x = x - 100
-        distancia_y = y - 100
-        d = math.sqrt(distancia_x**2 + distancia_y**2)
-        # Velocidade com base na distância do mouse
-        velocidade = min(10, d/10)
 
 class Canhao:
     def __init__(self):
@@ -38,32 +32,25 @@ class Canhao:
         # self.eixo_rotacao ser não mais no centro da imagem e sim a borda esquerda
         self.eixo_rotacao = (self.pos_canhao[0], self.pos_canhao[1] + 100)
         self.canhao_rotacionado = self.canhao_original
+        self.canhao_rect = self.canhao_rotacionado.get_rect(topleft=self.pos_canhao)
+
+
 
     def desenha_canhao(self, window):
         window.blit(self.canhao_rotacionado, self.pos_canhao)
+        #desenha o retangulo para me ajudar a entender a posicao 
+        pygame.draw.rect(window, (255, 0, 0), self.canhao_rect, 2)
+
 
     def rotaciona_canhao(self):
         mouse_x, mouse_y = pygame.mouse.get_pos()
         rel_x, rel_y = mouse_x - self.eixo_rotacao[0], mouse_y - self.eixo_rotacao[1]
         angle = math.degrees(math.atan2(-rel_y, rel_x))
         self.canhao_rotacionado = pygame.transform.rotate(self.canhao_original, angle)
+        # pega o rect que sempre muda com a rotacao
+        self.canhao_rect = self.canhao_rotacionado.get_rect(topleft=self.pos_canhao)
 
-class Passaro: 
-    def __init__(self):
-        img_passaro = pygame.image.load('assets\img/angry-birds-png.png')
-        self.passaro = pygame.transform.scale(img_passaro, (50,50))
-        self.rect_passaro = self.passaro.get_rect()
-        self.condicao_passaro = False
-
-    def desenha_disparo(self,window):
-        if self.condicao_passaro:
-            window.blit(self.passaro, self.rect_passaro)
-
-    def atira_disparo(self):
-        mouse_x, mouse_y = pygame.mouse.get_pos()
-        rel_x, rel_y = mouse_x - self.pos_disparo[0], mouse_y - self.pos_disparo[1]
-        angle = math.degrees(math.atan2(-rel_y, rel_x))
-        self.disparo = pygame.transform.rotate(self.disparo, angle)
+        return math.radians(angle)
 
 class TelaJogo(Jogo):
     def __init__(self):
@@ -74,34 +61,54 @@ class TelaJogo(Jogo):
     def desenha(self):
         self.window.blit(self.fundo, (0,0))
         self.canhao.desenha_canhao(self.window)
+        if self.condicao_passaro:
+            self.window.blit(self.passaro, self.rect_passaro)
+
     def update(self):
-
-        self.canhao.rotaciona_canhao()
-
+        angle = self.canhao.rotaciona_canhao()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+            elif event.type == pygame.MOUSEBUTTONDOWN and self.condicao_passaro == False:
+                self.condicao_passaro = True
+                # Ajusta a posição inicial do pássaro para a boca do canhão
+                self.rect_passaro.x = 100
+                self.rect_passaro.y = 600
+                
+                # Calcula o vetor de direção com base no ângulo de rotação do canhão
+                self.dir_x = math.cos(angle)
+                self.dir_y = -math.sin(angle)
+
+        if self.condicao_passaro:
+            # Move o pássaro na direção do vetor
+            self.rect_passaro.x += self.dir_x 
+            self.rect_passaro.y += self.dir_y 
+            print(self.rect_passaro.x, self.rect_passaro.y)
+
+            # Verifica se o pássaro saiu da tela
+            if not self.window.get_rect().colliderect(self.rect_passaro):
+                self.condicao_passaro = False
         return self 
 
-class Passaro_Projetil:
-    def __init__(self, origem_x, origem_y, destino_x, destino_y, velocidade):
-        self.x = origem_x
-        self.y = origem_y
-        self.destino_x = destino_x
-        self.destino_y = destino_y
-        self.velocidade = velocidade
+# class Passaro_Projetil:
+#     def __init__(self, origem_x, origem_y, destino_x, destino_y, velocidade):
+#         self.x = origem_x
+#         self.y = origem_y
+#         self.destino_x = destino_x
+#         self.destino_y = destino_y
+#         self.velocidade = velocidade
 
-    def movimento(self):
-        distancia_x = self.destino_x - self.x
-        distancia_y = self.destino_y - self.y
-        modulo = math.sqrt(distancia_x**2 + distancia_y**2)
-        if modulo > self.velocidade:
-            self.x += distancia_x * self.velocidade / d
-            self.y += distancia_y * self.velocidade / d
-        else:
-            self.x = self.destino_x
-            self.y = self.destino_y
+#     def movimento(self):
+#         distancia_x = self.destino_x - self.x
+#         distancia_y = self.destino_y - self.y
+#         modulo = math.sqrt(distancia_x**2 + distancia_y**2)
+#         if modulo > self.velocidade:
+#             self.x += distancia_x * self.velocidade / d
+#             self.y += distancia_y * self.velocidade / d
+#         else:
+#             self.x = self.destino_x
+#             self.y = self.destino_y
     
 tela_atual = TelaJogo()
 while True:
